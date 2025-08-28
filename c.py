b2d33d098de60d778b78064b6d847604eae28b76
PNyagol <nyagolpeter@gmail.com>
2025-08-28 11:11:44 +0300

    added socket.SOCK_STREAM

--- c.py
+++ c.py
@@ -4,7 +4,7 @@ from datetime import datetime
 host = "127.0.0.1"
 port = 7272
 
-with socket.socket(socket.AF_INET,) as s:
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
     s.connect((host, port))
     data = s.recv(1024)
     print(f"Received from server: {data.decode("utf-8")}")
