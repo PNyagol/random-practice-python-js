97bca90e366108f7f7be6fca9d24aa1db8a4ab2f
PNyagol <nyagolpeter@gmail.com>
2025-08-28 15:39:21 +0300

    wrapped accept() in a loop so the server stays alive

--- serverside.py
+++ serverside.py
@@ -8,8 +8,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((host, port))
     s.listen(5)
     print(f"Server is listening on {host}:{port}")
-    conn, addr = s.accept()
-
-    with conn:
-        connect_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S").encode()
-        conn.sendall(connect_time)
+    
+    while True:
+        conn, addr = s.accept()
+        
+        with conn:
+            print(f"Connected by {addr}")
+            connect_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S").encode()
+            conn.sendall(connect_time)
